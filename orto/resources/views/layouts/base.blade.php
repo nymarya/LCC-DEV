019e25bdad3a446e1976dfb2fa9f43d971876947
kathleenrego <rebeldes123***>
2018-02-15 17:15:48 -0300

    modificando menu

--- orto/resources/views/layouts/base.blade.php
+++ orto/resources/views/layouts/base.blade.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta content="Ortopedia" name="description">
+    <meta content="FisioCentro" name="description">
     <meta content="Kathleen e Italo" name="author">
-    <meta content="ortopedia" name="keywords">
+    <meta content="Fisioterapia" name="keywords">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'RET-SUS') | {{ config('app.name', 'RET-SUS') }}</title>
+    <title>@yield('title', 'FisioCENTRO') | {{ config('app.name', 'FisioCENTRO') }}</title>
 
     <!-- Styles -->
     <link href="{{asset('/css/app.css')}}" rel="stylesheet">
@@ -20,7 +20,7 @@
     <link rel="stylesheet" href="{{asset('/css/AdminLTE.min.css')}}">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
          folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="{{asset('/css/skin-red.min.css')}}">skin-red.min.css
+    <link rel="stylesheet" href="{{asset('/css/skin-red.min.css')}}">
 @stack('stylesheets')
 
 <!-- Scripts -->
