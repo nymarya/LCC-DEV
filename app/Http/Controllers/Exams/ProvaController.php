0b1035b5368d37698ed739a096dedd175ddcae1f
Mayra Dantas <mayradazevedo@gmail.com>
2018-02-22 09:37:43 -0300

    cria projeto controller

---
+++ app/Http/Controllers/Exams/ProvaController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Exams;
+
+use App\Http\Controllers\Controller;
+use App\Models\Questao;
+use Illuminate\Http\Request;
+
+class ProvaController extends Controller
+{
+    protected $request;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->middleware('role:professor');
+        $this->request = $request;
+    }
+
+    public function index(){
+        return view('provas.index', [
+            'provas' => Questao::whereNull('deleted_at')->get()
+        ]);
+    }
+
+    public function create(){
+        return view('provas.create',[
+            'turmas' => 'aa',
+            'questoes' => 'aa',
+        ]);
+    }
+
+    public function store() {
+        $this->validate($this->request, [
+            'questao' => ['required'],
+            'alternativas' => ['required', 'array'],
+            'alternativas.*.alternativa' => ['required'],
+        ]);
+
+        $questao = Questao::create($this->request->only('questao'));
+
+        $alternativas = collect($this->request->get('alternativas'))->map(function ($item, $key) {
+            if($item['correta'] == null){
+                $item['correta'] = false;
+            }
+
+            return $item;
+        })->toArray();
+
+        $questao->alternativas()
+            ->createMany($alternativas);
+
+        //continua na mesma tela para facilitar o cadastro de varias questoes
+        return redirect()->route('questoes.create')
+            ->with('success', 'Questão cadastrada com sucesso!');
+
+    }
+
+    public function show($id){
+        $questao = Questao::findOrFail($id);
+
+        return view('questoes.show', [
+            'questao' => $questao
+        ]);
+    }
+
+    public function destroy($id){
+        Questao::findOrFail($id)->delete();
+
+        return redirect()->route('questoes.index')->with('success', 'Questão excluída com sucesso');
+    }
+}
